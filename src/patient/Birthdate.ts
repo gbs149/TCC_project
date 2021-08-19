10c72a400729cf922cdf19bd128a4aed1f5c43c0
Gabriel Schmitt <gabriel.schmitt@magrathealabs.com>
2021-08-19 09:31:40 -0300

    Add Birthdate with validation

---
+++ src/patient/Birthdate.ts
@@ -0,0 +1,17 @@
+import { isPast } from "date-fns";
+
+export class Birthdate {
+  date: Date;
+
+  private constructor(date: Date) {
+    this.date = date;
+  }
+
+  static create(date: Date): Birthdate {
+    if (isPast(date)) {
+      return new Birthdate(date);
+    } else {
+      throw Error("Birthdate must be in the past");
+    }
+  }
+}
