a8ee3c549d9f0d4b730a3f4550b7f3b80a53144d
Gabriel Schmitt <gabriel.schmitt@magrathealabs.com>
2021-09-18 11:40:04 -0300

    Fix phone validation

--- src/patient/validation/phoneValidation.ts
+++ src/patient/validation/phoneValidation.ts
@@ -9,6 +9,6 @@ const parsePhoneNumber = (num: string): PhoneNumber =>
   parsePhoneNumberFromString(num, "BR", metadata as unknown as Metadata);
 
 const isValidPhoneNumber = (num: string): boolean =>
-  parsePhoneNumber(num).isValid();
+  parsePhoneNumber(num)?.isValid() ?? false;
 
 export { isValidPhoneNumber };
