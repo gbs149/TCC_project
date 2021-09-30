d4c60ffed9f44dea448a7923d108138c1f2d1c80
Gabriel Schmitt <gabriel.schmitt@magrathealabs.com>
2021-09-30 09:25:54 -0300

    Add basic server functionality

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import Koa from "koa";
 
 import Router from "@koa/router";
+import bodyParser from "koa-bodyparser";
 
 import { patientRouter } from "./patient";
 
 const router = new Router();
 
-router.use(patientRouter.routes()).use(patientRouter.allowedMethods());
+router.use("/patient", patientRouter.routes(), patientRouter.allowedMethods());
 
 export const app = new Koa();
 
+app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.on("error", function (error: Error) {
