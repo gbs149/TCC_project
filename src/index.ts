9171b16487afefa6ac264429ae886b5801db09a5
Gabriel Schmitt <gabriel.schmitt@magrathealabs.com>
2021-09-30 09:25:54 -0300

    Add basic server functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { app } from "./app";
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port);
+console.log(`Started on port ${port}`);
+
+process.on("SIGINT", () => {
+  console.warn("stopped accepting new connections");
+  server.close(async () => {
+    console.info("closed");
+    // await database.close();
+    process.exit(0);
+  });
+});
--- src/patient/index.ts
+++ src/patient/index.ts
@@ -1,4 +1,5 @@
 import Router from "@koa/router";
+import { match } from "fp-ts/lib/Either";
 import { PatientDTO } from "./DTOs/PatientDTO";
 import { createPatient } from "./model/PatientModel";
 
@@ -13,18 +14,43 @@ export const patientRouter = new Router();
 patientRouter
   .get("/", async (ctx) => {
     try {
-      ctx.body = "PATIETN";
-      console.log("patient", ctx.body);
+      ctx.body = ["PATIETNS"];
+      console.log("GET patients", ctx.body);
     } catch (e) {
       console.error(e);
     }
+  })
+  .get("/:id", async (ctx) => {
+    const { id } = ctx.params;
+    ctx.body = `GET PATIENT ${id}`;
+    console.log(`GET PATIENT ${id}`);
   })
   .post("/", async (ctx) => {
     const patientDTO: PatientDTO = ctx.request.body;
 
     const patient = createPatient(patientDTO);
 
-    console.log(patient);
+    match(
+      (errors) => {
+        ctx.status = 400;
+        ctx.body = errors;
+      },
+      (patientModel) => {
+        ctx.status = 200;
+        ctx.body = patientModel;
+      }
+    )(patient);
+  })
+  .delete("/:id", async (ctx) => {
+    const { id } = ctx.params;
 
-    ctx.body = "PATTIIEN POST";
+    ctx.body = `DELETE ${id}`;
+    console.log(id);
+  })
+  .patch("/:id", async (ctx) => {
+    const { id } = ctx.params;
+    const patientDTO: PatientDTO = ctx.request.body;
+
+    ctx.body = patientDTO;
+    console.log(`PATCH ${id}`, patientDTO);
   });
