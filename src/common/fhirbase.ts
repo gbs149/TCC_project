a4815eab999d68f408728eb7cbc8715237c82d2d
Gabriel Schmitt <gabriel.schmitt@magrathealabs.com>
2021-10-03 07:34:40 -0300

    Add PatientController

--- src/common/fhirbase.ts
+++ src/common/fhirbase.ts
@@ -1,4 +1,4 @@
-import { Resource } from "fhir/r2";
+import { Resource } from "fhir/r4";
 import { Pool } from "pg";
 
 const pool = new Pool();
@@ -26,10 +26,10 @@ const update =
     return query.rows[0].fhirbase_update;
   };
 
-const del = async (pool: Pool) => async (resource: Resource) => {
+const del = (pool: Pool) => (resourceType: string) => async (id: string) => {
   const query = await pool.query("SELECT fhirbase_delete($1, $2)", [
-    resource.resourceType,
-    resource.id,
+    resourceType,
+    id,
   ]);
   return query.rows[0].fhirbase_delete;
 };
@@ -39,8 +39,11 @@ const getById =
   (entity: string) =>
   async (id: string): Promise<Resource> => {
     const response = await pool.query(
-      `SELECT * FROM public.${entity} AS entity 
-     WHERE entity.id = $1 ORDER BY txid DESC LIMIT 1;`,
+      `SELECT *
+                     FROM public.${entity} AS entity
+                     WHERE entity.id = $1
+                     ORDER BY txid DESC
+                     LIMIT 1;`,
       [id]
     );
 
@@ -48,22 +51,31 @@ const getById =
   };
 
 const getAll =
-  (pool: Pool) =>
-  async (entity: string): Promise<Resource[]> => {
+  (pool: Pool) => (entity: string) => async (): Promise<Resource[]> => {
     const response = await pool.query(
-      `SELECT * FROM public.${entity} AS entity 
-     ORDER BY txid DESC;`
+      `SELECT *
+             FROM public.${entity} AS entity
+             ORDER BY txid DESC;`
     );
     return response.rows;
   };
 
-const createFhirbase = (pool: Pool) => ({
-  getById: getById(pool),
-  getAll: getAll(pool),
+const createFhirbase = (pool: Pool, entity: string): Fhirbase => ({
+  getById: getById(pool)(entity),
+  getAll: getAll(pool)(entity),
   create: create(pool),
   update: update(pool),
-  delete: del(pool),
-  query: pool.query.bind(pool),
+  delete: del(pool)(entity),
 });
 
-export const fhirbase = createFhirbase(pool);
+interface Fhirbase {
+  getById: (id: string) => Promise<Resource>;
+  getAll: () => Promise<Resource[]>;
+  create: (r: Resource) => Promise<Resource>;
+  update: (r: Resource) => Promise<Resource>;
+  // TODO remove any
+  delete: (id: string) => Promise<any>;
+}
+
+export const fhirbase = (entity: string): Fhirbase =>
+  createFhirbase(pool, entity);
